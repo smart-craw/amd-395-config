f02b2f52bb5dbf13c60ec7170ac498e3af02c61c
Daniel Stahl <danstahl1138@gmail.com>
2026-09-07 13:42:24 -0500

    added network

--- halogen-qwen3.8-flash.sh
+++ halogen-qwen3.8-flash.sh
@@ -1,4 +1,7 @@
-podman run --rm -p 8731:8731 \
+#!/bin/bash
+# see qwen-next.md for network creation
+podman run --rm --network webserver-net \
+  -p 8731:8731 \
   --device /dev/kfd --device /dev/dri --group-add keep-groups \
   --security-opt seccomp=unconfined --ipc=host --ulimit memlock=-1:-1 \
   -v ~/models/qwen3.8-flash-halogen:/models:ro \
